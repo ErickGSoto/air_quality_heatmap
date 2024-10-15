219bf5fa9cc2059a4176891702b1f72da29fe748
Erick Gutierrez <erick.gutierre@oracle.com>
2024-10-14 23:59:33 -0600

    Push first files

---
+++ src/main/java/com/erickgsoto/airquality/heatmap/AirQualityHeatmapApplication.java
@@ -0,0 +1,13 @@
+package com.erickgsoto.airquality.heatmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityHeatmapApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AirQualityHeatmapApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/erickgsoto/airquality/heatmap/AirQualityHeatmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickgsoto.airquality.heatmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityHeatmapApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
